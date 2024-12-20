0df04c393d14077fb36810b992e388bcb4a7e438
Gayathrichandrashekar <125109035+Gayathrichandrashekar@users.noreply.github.com>
2024-12-20 13:25:01 +0530

    Create addition.py

---
+++ src/addition.py
@@ -0,0 +1,8 @@
+$ git clone https://github.com/Mahi2708/sample-programs.git
+$ cd sample-programs/
+$ cat README.md
+$ nano README.md
+$ git add .
+$git commit -m  "committed2"
+$ git push origin master/main
+
